6b629d892923d44a9927f2e47cf3fd9daef086de
Garland Xie <garlandxie@gmail.com>
2024-10-18 16:44:47 -0400

    Add more data cleaning

--- src/02-adjust_sample_eff.R
+++ src/02-adjust_sample_eff.R
@@ -1,8 +1,9 @@
 # libraries --------------------------------------------------------------------
-library(iNEXT) # for adjusting sampling efforts in GBIF data
-library(here)  # for creating relative file-paths
-library(sf)    # for manipulating geospatial files
-library(dplyr) # for manipulating data
+library(iNEXT)    # for adjusting sampling efforts in GBIF data
+library(here)     # for creating relative file-paths
+library(sf)       # for manipulating geospatial files
+library(dplyr)    # for manipulating data
+library(ggplot2)  # for visualizing data
 
 # import -----------------------------------------------------------------------
 occ_tidy <- read.csv(here("data", "intermediate_data", "occ_tidy.csv"))
@@ -19,7 +20,7 @@ ugs_dissolved <- ugs %>%
   st_transform(crs = 32617) %>%
   mutate(id = c(1:nrow(.)))
 
-# get species by sampling units for incidence raw data
+# transform GBIF records into species-site matrix for iNEXT
 occ_crs <- occ_tidy %>%
   st_as_sf(coords = c("decimalLongitude", "decimalLatitude")) %>%
   st_set_crs(4326) %>%
@@ -35,15 +36,12 @@ occ_ind <- ugs_dissolved %>%
   # aggregate across all years (for now)
    dplyr::filter(!is.na(year)) %>%
    mutate(
-    
     occurrenceStatus = case_when(
       occurrenceStatus == "PRESENT" ~ "1", 
       occurrenceStatus == "ABSENT" ~ "0", 
       TRUE ~ occurrenceStatus), 
     occurrenceStatus = as.numeric(occurrenceStatus)
-        
   ) %>%
-  
   group_by(id, species) %>%
   summarize(num_records = sum(occurrenceStatus)) %>%
   ungroup() %>%
@@ -58,6 +56,41 @@ occ_ind <- ugs_dissolved %>%
 next_gbif <- iNEXT(x = occ_ind, datatype = "abundance", q = 0)
 
 # remove single records
-next_gbif_summ <- dplyr::filter(next_gbif$DataInfo, n > 1) 
-  
+next_gbif_summ <- next_gbif$DataInfo  %>%
+  dplyr::select(
+    id = Assemblage, 
+    num_gbif_records = n, 
+    species_richness = S.obs, 
+    sample_coverage = SC
+    ) %>%
+  mutate(
+    id = as.integer(id)
+  )
+                
+# find sites with high sample coverage -----------------------------------------
+
+ugs_inext <- full_join(ugs_dissolved, next_gbif_summ, by = "id")
+
+plot_inext <- ugs_inext %>%
+  dplyr::filter(num_gbif_records >1 & species_richness >1) %>%
+  ggplot() + 
+    geom_sf(aes(fill = sample_coverage)) + 
+    scale_fill_distiller(name = "SC", palette = "Spectral") + 
+    theme_bw()
+
+# save to disk -----------------------------------------------------------------
+
+st_write(
+  obj = ugs_inext, 
+  dsn = here("data", "intermediate_data", "ugs_inext.shp")
+  )
+
+ggsave(
+  filename = here("plot_inext.png"),
+  plot = plot_inext, 
+  device = "png",
+  units = "in",
+  height = 6, 
+  width = 6  
+)
                      
