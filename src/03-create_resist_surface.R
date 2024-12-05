51d1c45d91240178c3cf0f6c47cac709108c6fe8
garlandxie <garlandxie@gmail.com>
2024-12-05 11:44:43 -0500

    Import elevation data

--- src/03-create_resist_surface.R
+++ src/03-create_resist_surface.R
@@ -6,6 +6,8 @@ library(raster)           # for manipulating raster data
 
 # import ----
 
+## forest and land cover 2018 ----
+
 # note: the Forest and Land Cover 2018 data was rasterized by N. Sookhan
 # using QGIS in the BUGS lab at the University of Toronto Scarborough
 # this process is probably much faster to do in QGIS than R given 
@@ -14,4 +16,25 @@ lc_2018 <- raster(
   here("data", "input_data", 
        "resist_surf", "to_lc_2018", "to_lc_2018.tif")
   )
+
+## elevation ----
+
+# metadata from open.toronto.ca/dataset/triangular-irregular-network-tin
+
+# Triangular Irregular Network (TIN) polygon elevation data
+# derived from 2023 LiDAR-derived 2.5 m resolution bare-earth elevation 
+
+# attributes:
+# Avg_Elev: Average Elevation of the polygon
+# Aspect: Directional measure of slope
+# Slope: Rate of change of elevation at a surface location in degrees
+# Ptg_Slope: Rate of change of elevation at surface location as percentage
+
+tin_elev <- show_package("4ab91af6-6b08-4318-aee0-87f163891e53") %>%
+  dplyr::filter(row_number()==1) %>%
+  list_package_resources() %>%
+  get_resource()
+
+
+
 
