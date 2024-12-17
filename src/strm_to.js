9257833c006334c6e8fe8f231b04c7c1200d0e27
Garland Xie <garlandxie@gmail.com>
2024-12-17 10:47:32 -0500

    Update strm_to.js

--- src/strm_to.js
+++ src/strm_to.js
@@ -1,41 +1,28 @@
-// Get the boundary of Toronto
-var geometry = 
-    /* color: #d63000 */
-    /* displayProperties: [
-      {
-        "type": "rectangle"
-      },
-      {
-        "type": "rectangle"
-      }
-    ] */
-    ee.Geometry.MultiPolygon(
-        [[[[-79.67199031180144, 43.87844835367451],
-           [-79.67199031180144, 43.558231140741036],
-           [-79.10070124930144, 43.558231140741036],
-           [-79.10070124930144, 43.87844835367451]]],
-         [[[-79.53500453299284, 43.71352873374248],
-           [-79.53500453299284, 43.713032421571846],
-           [-79.52333155936003, 43.713032421571846],
-           [-79.52333155936003, 43.71352873374248]]]], null, false);
-
-// Import STRM dataset
+// Import data
 var dataset = ee.Image('CSP/ERGo/1_0/Global/SRTM_topoDiversity');
+
+// Clip to boundary of TO
+var geometry = ee.FeatureCollection("projects/ee-garlandxie/assets/to_boundary")
+                 .geometry()
+
+// Visualize data                
 var srtmTopographicDiversity = dataset.select('constant').clip(geometry);
 var srtmTopographicDiversityVis = {
   min: 0,
   max: 1,
 };
-
-// Visualize
 Map.setCenter(-79.3832, 43.6532, 9);
 Map.addLayer(
     srtmTopographicDiversity, srtmTopographicDiversityVis,
     'SRTM Topographic Diversity');
 
-// Export to Google Drive
+// Export to Google Drive 
+var crs_utm_17N = 'PROJCS["NAD83 / UTM zone 17N",GEOGCS["NAD83",DATUM["North_American_Datum_1983",SPHEROID["GRS 1980",6378137,298.257222101],TOWGS84[0,0,0,0,0,0,0]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4269"]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",-81],PARAMETER["scale_factor",0.9996],PARAMETER["false_easting",500000],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Easting",EAST],AXIS["Northing",NORTH],AUTHORITY["EPSG","26917"]]';
+
 Export.image.toDrive({
   image: srtmTopographicDiversity,
-  description: 'strm_toronto_30m_res',
-  scale: 30
+  crs: crs_utm_17N, 
+  description: 'strm_to_3m_res',
+  maxPixels: 145309360,
+  scale: 3
 })
