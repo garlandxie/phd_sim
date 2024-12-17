3e5402842151d51c1374d42521d0fb2bf58fe270
Garland Xie <garlandxie@gmail.com>
2024-12-17 16:13:58 -0500

    Update strm_to.js

--- src/strm_to.js
+++ src/strm_to.js
@@ -3,10 +3,11 @@ var dataset = ee.Image('CSP/ERGo/1_0/Global/SRTM_topoDiversity');
 
 // Clip to boundary of TO
 var geometry = ee.FeatureCollection("projects/ee-garlandxie/assets/to_boundary")
-                 .geometry()
+                 .geometry();
+                 
+var srtmTopographicDiversity = dataset.select('constant').clip(geometry);
 
 // Visualize data                
-var srtmTopographicDiversity = dataset.select('constant').clip(geometry);
 var srtmTopographicDiversityVis = {
   min: 0,
   max: 1,
@@ -17,12 +18,12 @@ Map.addLayer(
     'SRTM Topographic Diversity');
 
 // Export to Google Drive 
-var crs_utm_17N = 'PROJCS["NAD83 / UTM zone 17N",GEOGCS["NAD83",DATUM["North_American_Datum_1983",SPHEROID["GRS 1980",6378137,298.257222101],TOWGS84[0,0,0,0,0,0,0]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4269"]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",-81],PARAMETER["scale_factor",0.9996],PARAMETER["false_easting",500000],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Easting",EAST],AXIS["Northing",NORTH],AUTHORITY["EPSG","26917"]]';
-
 Export.image.toDrive({
   image: srtmTopographicDiversity,
-  crs: crs_utm_17N, 
+  crs: 'EPSG:26917',
   description: 'strm_to_3m_res',
-  maxPixels: 145309360,
-  scale: 3
-})
+  folder: "google earth engine",
+  fileFormat: 'GeoTIFF',
+  maxPixels: 209242992,
+  scale: 2.5
+});
