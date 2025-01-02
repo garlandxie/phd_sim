da1092c697832915cd29f802387eabf0a7892224
Garland Xie <garlandxie@gmail.com>
2025-01-02 10:58:40 -0500

    Move raster algebra workflow to a new script

--- src/01-create_scenarios/assign_green_space_types.R
+++ src/01-create_scenarios/assign_green_space_types.R
@@ -7,7 +7,6 @@ library(sf)               # for manipulating geospatial (vector) data
 library(raster)           # for manipulating geospatial (raster) data  
 library(exactextractr)    # for calculating zonal statistics
 library(ggplot2)          # for visualizing data 
-library(fasterize)        # for rasterizing polygons
 
 # import data ----
 
@@ -58,7 +57,10 @@ ndvi_3m <- raster(here(
 )
 
 ## parking lots ----
-pl <- read_sf(here("data", "intermediate_data", "parking-lots-in-parkland-priority.shp"))
+pl <- read_sf(here(
+  "data", "intermediate_data", 
+  "parking-lots-in-parkland-priority.shp")
+  )
 
 # clean data -----
 
@@ -151,28 +153,9 @@ pl_tidy <- pl_transform %>%
     windspeed
   )
 
-## reclassify land cover ----
-
-# convert into terra class objects
-pl_r_lc <- terra::vect(pl_tidy)
-lc_spatrast <- terra::rast(lc_3m)
-lc_spatrast2 <- terra::rast(lc_3m) # to avoid overwriting lc_spatrast
-
-# remove color table for each landcover raster
-terra::coltab(lc_spatrast) <- NULL
-terra::coltab(lc_spatrast2) <- NULL
-
-# create a raster with assigned green spaces values for each parking lot
-pl_r <- terra::rasterize(pl_r_lc, lc_spatrast, field = "landcover")
-
-# use raster algebra to replace impervious surface values
-# with green spaces values for each parking lot 
-lc_spatrast2[!is.na(pl_r[])] <- pl_r[!is.na(pl_r[])]
-
 # save to disk -----
 
-## reclassify land cover ----
-writeRaster(
-  x = lc_spatrast2, 
-  filename = here("data", "intermediate_data", "lc_reclassify.tiff")
-  )
+st_write(
+  obj = pl_tidy, 
+  dsn = here("data", "intermediate_data", "pl_green_space_type.shp")
+)
