64b26e950de221b8622149ddfc8511050b468cc6
Garland Xie <garlandxie@gmail.com>
2025-01-10 13:12:36 -0500

    Initial commit

---
+++ src/02-create_resist_surface/create_target_group.R
@@ -0,0 +1,84 @@
+# libraries --------------------------------------------------------------------
+library(rgbif)    # for querying GBIF records
+library(here)     # for creating relative file-paths
+library(sf)       # for manipulating geospatial files
+library(wk)       # for manipulating WTK polygons - needed for GBIF queries
+
+# import -----------------------------------------------------------------------
+
+# load green space dataset from City of Toronto 
+# this is done to query the GBIF occurrence records using a polygon
+ugs <- sf::read_sf(here("data", "input_data", "green_spaces_4326"))
+
+# get occurrence records (with geospatial coordinates)
+# of species within the Apocynaceae family
+# the collective records of all species within this family
+# will form the target-ground background option
+keys_id <- rgbif::name_backbone_checklist(
+  c("Apocynum androsaemifolium",
+  "Apocynum cannabinum",
+  "Asclepias exaltata", 
+  "Asclepias incarnata",
+  "Asclepias sullivantii",
+  "Asclepias syriaca",
+  "Asclepias tuberosa",
+  "Gonolobus niger",
+  "Vinca minor",
+  "Vincetoxicum nigrum",
+  "Vincetoxicum rossicum"
+  )
+)
+
+# create WTK polygon of the City of Toronto boundary
+# this polygon should reduce the number of imported queries from GBIF
+wkt_to_boundary <- ugs %>%
+  sf::st_bbox() %>%
+  sf::st_as_sfc() %>%
+  sf::st_as_text() %>%
+  wk::wkt() %>%
+  wk::wk_orient()
+
+# run GBIF query from API
+occ_download(
+  pred_in("speciesKey", keys_id$speciesKey),
+  pred("hasGeospatialIssue", FALSE),
+  pred("hasCoordinate", TRUE),
+  pred("occurrenceStatus","PRESENT"), 
+  pred_within(wkt_to_boundary)
+)
+
+to_occ <- occ_download_get(
+  key = '0061415-241126133413365',
+  path = here("data", "input_data"),
+  overwrite = TRUE
+) %>%
+  occ_download_import(
+    select = c(
+      "gbifID", 
+      "occurrenceID", 
+      "occurrenceStatus",
+      "year", 
+      "decimalLatitude", 
+      "decimalLongitude",
+      "coordinateUncertaintyInMeters",
+      "species", 
+      "acceptedScientificName")
+  )
+
+
+# save to disk ----
+
+ggsave(
+  filename = here("output", "data_appendix", "GBIFrecs_trgtgrp.png"), 
+  plot = plot_trgtgrp, 
+  device = "png", 
+  units = "in", 
+  height = 5, 
+  width = 7
+)
+
+  
+ 
+
+
+
