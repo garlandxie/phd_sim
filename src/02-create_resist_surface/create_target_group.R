3ea05f23cf4b06831b949ed69b621b01f929e007
Garland Xie <garlandxie@gmail.com>
2025-01-13 14:20:48 -0500

    Add more data cleaning

--- src/02-create_resist_surface/create_target_group.R
+++ src/02-create_resist_surface/create_target_group.R
@@ -3,7 +3,9 @@ library(rgbif)    # for querying GBIF records
 library(here)     # for creating relative file-paths
 library(sf)       # for manipulating geospatial files
 library(wk)       # for manipulating WTK polygons - needed for GBIF queries
-library(janitor)
+library(janitor)  # for cleaning column names
+library(opendatatoronto)
+library(dplyr)
 
 # import -----------------------------------------------------------------------
 
@@ -76,15 +78,33 @@ to_occ <- occ_download_get(
 duplicates <- janitor::get_dupes(to_occ)
 
 # remove focal species (Vincetoxicum rossicum)
-trgt_grp <- dplyr::filter(to_occ, species != "Vincetoxicum rossicum")
+trgt_grp <- dplyr::filter(to_occ, species != "Vincetoxicum rossicum") 
+
+# get occurrence records within Toronto boundary
+to_bound <- list_package_resources("841fb820-46d0-46ac-8dcb-d20f27e57bcc") %>%
+  get_resource()
+
+to_bound_utm <- st_transform(to_bound, 32617)
+
+trgt_grp_sf <- trgt_grp %>%
+  st_as_sf(coords = c("decimalLongitude", "decimalLatitude"), crs = "EPSG:4326") %>%
+  st_transform(32617) %>%
+  mutate(
+    lon = sf::st_coordinates(.)[,1], 
+    lat = sf::st_coordinates(.)[,2]
+  )
+
+trgt_grp_2 <- st_intersection(trgt_grp_sf, to_bound_utm) %>%
+  st_drop_geometry()
+  
 
 # create target group bias raster ----
 
 # aggregate by coordinates
-sum_records <- as.data.frame(dplyr::count(trgt_grp, decimalLatitude, decimalLongitude))
+sum_records <- as.data.frame(dplyr::count(trgt_grp_2, lat, lon))
 
 # extract coordinates
-coords <- cbind(sum_records[, "decimalLatitude"], sum_records[, "decimalLongitude"])
+coords <- cbind(sum_records[, "lat"], sum_records[, "lon"])
 
 # do a 2D kernel density estimation
 target_density <- ks::kde(coords)
@@ -93,10 +113,10 @@ target_density <- ks::kde(coords)
 target_raster <- raster::raster(target_density)
 
 # define in UTM 17N
-crs(target_raster) <- '+init=EPSG:26917'
+raster::crs(target_raster) <- '+init=EPSG:32617'
 
 # 
-target_raster <- target_raster - minValue(target_raster)
+target_raster <- target_raster - raster::minValue(target_raster)
 target_raster <- terra::rast(target_raster)
 target_raster <- spatialEco::raster.transformation(target_raster, trans="norm")
 
@@ -110,9 +130,3 @@ ggsave(
   height = 5, 
   width = 7
 )
-
-  
- 
-
-
-
