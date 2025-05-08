ca7adbda40141380520c678508434374037fd566
Garland Xie <garlandxie@gmail.com>
2025-05-08 10:58:54 -0400

    Clean up the script so it's mainly inverting the raster values of the SDMs

--- src/02-create_resist_surface/05-create_resist_surface.R
+++ src/02-create_resist_surface/05-create_resist_surface.R
@@ -7,77 +7,42 @@ library(sf)           # for importing ESRI shapefiles
 
 # import -----------------------------------------------------------------------
 
-## urban green spaces ----------------------------------------------------------
-
-to_ugs <- read_sf(here("data/input_data/green_spaces_4326/Green Spaces - 4326.shp"))
-
-pl <- read_sf(here(
-  "data", "intermediate_data", "parking lots",
-  "pl_green_space_type.shp"))
-
-## SDM rasters -----------------------------------------------------------------
-sc1_hsm_map <- terra::rast(here(
-  "syncrosim/ugs_simulations.ssim.data/Scenario-83/wisdm_OutputSpatial/maxent_prob_map.tif")
+sc1_sdm <- terra::rast(here(
+  "data/intermediate_data/maxent_sdm/DSV.tgb/Final/CurrentEM_final_tgb.tif")
   )
 
-sc2_hsm_map <- terra::rast(here(
-  "syncrosim/ugs_simulations.ssim.data/Scenario-87/wisdm_OutputSpatial/maxent_prob_map.tif")
+sc2_sdm <- terra::rast(here(
+  "data/intermediate_data/maxent_sdm/DSV.tgb/Final/FutureEM_final_tgb.tif")
 )
 
-tgt_back <-  terra::rast(here(
-  "data/intermediate_data/target_background/trgt_prob_raster.tiff")
-)
+# clean: resistance surface ----------------------------------------------------
 
-
-# clean ------------------------------------------------------------------------
-
-## resistance surface ----------------------------------------------------------
-sc1_resist_surf <- spatialEco::raster.invert(sc1_hsm_map)
-sc2_resist_surf <- spatialEco::raster.invert(sc2_hsm_map)
-
-## to ugs ----------------------------------------------------------------------
-ugs_utm17n <- st_transform(to_ugs, 26917)
+sc1_resist_surf <- spatialEco::raster.invert(sc1_sdm)
+sc2_resist_surf <- spatialEco::raster.invert(sc2_sdm)
 
 # visualize --------------------------------------------------------------------
 
-(tgt_back_map <- ggplot() + 
-   tidyterra::geom_spatraster(data = tgt_back) +   
-   guides(fill = guide_legend(title = "Probability Surface")) + 
-   scale_fill_gradientn(
-     colours = terrain.colors(10),
-     na.value = NA) + 
-   theme_bw()
-   )
-
-(maxent_prob_map <- ggplot() + 
-  tidyterra::geom_spatraster(data = maxent_prob_rast) + 
-  guides(fill = guide_legend(title = "Habitat suitability of V. rossicum")) +
-  scale_fill_viridis_c(
-    option = "magma",
-    breaks = c(0, 20, 40, 60, 80, 100),
-    na.value = NA
-    ) +
-  theme_bw() 
-)
-
-(resist_surf_map <- ggplot() + 
-    tidyterra::geom_spatraster(data = resist_surf) + 
+(sc1_resist_surf_map <- ggplot() + 
+    tidyterra::geom_spatraster(data = sc1_resist_surf) + 
     guides(fill = guide_legend(title = "Resistance Surface")) +
     scale_fill_viridis_c(
       option = "magma",
       na.value = NA,
       breaks = c(0, 20, 40, 60, 80, 100)
-    ) 
+    ) + 
+   theme_bw()
 )
 
-new_ugs_sc <- ggplot() + 
-  geom_sf(data = ugs_utm17n) + 
-  geom_sf(data = pl, col = "red") +
-  theme_bw()
-
-existing_ugs_sc <- ggplot() + 
-  geom_sf(data = ugs_utm17n) + 
-  theme_bw()
+(sc2_resist_surf_map <- ggplot() + 
+    tidyterra::geom_spatraster(data = sc2_resist_surf) + 
+    guides(fill = guide_legend(title = "Resistance Surface")) +
+    scale_fill_viridis_c(
+      option = "magma",
+      na.value = NA,
+      breaks = c(0, 20, 40, 60, 80, 100)
+    ) + 
+    theme_bw()
+)
 
 # save to disk -----------------------------------------------------------------
 
@@ -87,67 +52,15 @@ terra::writeRaster(
   x = sc1_resist_surf, 
   filename = here("data", "intermediate_data", "resist_surfaces",
                   "sc1_resist_surf.tif"),
-  gdal=c("COMPRESS=DEFLATE", "TFW=YES")
+  gdal=c("COMPRESS=DEFLATE", "TFW=YES"),
+  overwrite = TRUE
 )
 
 terra::writeRaster(
   x = sc2_resist_surf, 
   filename = here("data", "intermediate_data", "resist_surfaces",
                   "sc2_resist_surf.tif"),
-  gdal=c("COMPRESS=DEFLATE", "TFW=YES")
+  gdal=c("COMPRESS=DEFLATE", "TFW=YES"),
+  overwrite = TRUE
 )
 
-
-ggsave(
-  plot = maxent_prob_map, 
-  filename = here("output", "maxent_prob_map.png"),
-  device = "png", 
-  units = "in", 
-  width = 8, 
-  height = 8
-)
-
-ggsave(
-  plot = resist_surf_map, 
-  filename = here("output", "resist_surface.png"),
-  device = "png", 
-  units = "in", 
-  width = 8, 
-  height = 8
-)
-
-ggsave(
-  plot = tgt_back_map, 
-  filename = here("output", "tgt_back_map.png"),
-  device = "png", 
-  units = "in", 
-  width = 8, 
-  height = 8
-)
-
-ggsave(
-  plot = tgt_back_map, 
-  filename = here("output", "tgt_back_map.png"),
-  device = "png", 
-  units = "in", 
-  width = 8, 
-  height = 8
-)
-
-ggsave(
-  plot = new_ugs_sc, 
-  filename = here("output", "new_ugs_scenario.png"),
-  device = "png", 
-  units = "in", 
-  width = 8, 
-  height = 8
-)
-
-ggsave(
-  plot = existing_ugs_sc, 
-  filename = here("output", "existing_ugs_scenario.png"),
-  device = "png", 
-  units = "in", 
-  width = 8, 
-  height = 8
-)
