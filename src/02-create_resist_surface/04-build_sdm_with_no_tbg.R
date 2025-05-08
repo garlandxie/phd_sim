3a997dd5785163e75f072d7fe03a8b0ce63aecdf
Garland Xie <garlandxie@gmail.com>
2025-05-08 10:15:35 -0400

    Update contact info

--- src/02-create_resist_surface/04-build_sdm_with_no_tbg.R
+++ src/02-create_resist_surface/04-build_sdm_with_no_tbg.R
@@ -11,9 +11,7 @@
 #   (1) Department of Biological Sciences, 
 #       University of Toronto Scarborough,
 #       1265 Military Trail, Toronto, ON, M1C 1A4, Canada
-#       email: garland.xie@mail.utoronto.ca, 
-#              nicholas.sookhan@mail.utoronto.ca
-#              scott.macivor@mail.utoronto.ca
+#       email: garlandxie@gmail.com
 #   (2) Department of Ecology and Evolutionary Biology,
 #       University of Colorado Boulder,
 #
