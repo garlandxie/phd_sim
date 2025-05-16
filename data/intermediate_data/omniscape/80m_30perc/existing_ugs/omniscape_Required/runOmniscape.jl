9fc8e451a0c32626cda06153ad20a7f837933a51
Garland Xie <garlandxie@gmail.com>
2025-05-16 14:49:07 -0400

    Initial commit

---
+++ data/intermediate_data/omniscape/80m_30perc/existing_ugs/omniscape_Required/runOmniscape.jl
@@ -0,0 +1,5 @@
+cd(raw"C:\Users\Garland\Documents\R\phd_sim\syncrosim\ugs_sim.ssim.data\Scenario-113\omniscape_Required")
+
+using Pkg; Pkg.add(name="GDAL"); Pkg.add(name="Omniscape")
+using Omniscape
+run_omniscape("config.ini")
